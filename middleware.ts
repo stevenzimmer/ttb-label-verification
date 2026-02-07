a11881175bc23805b7b5975f5c1dd1ca0119ea4f
Steven Zimmer <webdevzim@gmail.com>
2026-02-06 20:31:35 -0800

    add vercel config

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    matcher: [
-        // Run middleware on everything EXCEPT Next internals and common static files
-        "/((?!_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.*\\.(?:png|jpg|jpeg|gif|webp|svg|ico|css|js|map|txt|woff|woff2|ttf|eot)$).*)",
-    ],
-};
