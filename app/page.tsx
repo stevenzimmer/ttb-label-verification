5c3379e6118c62fdc9fc67a46e4352b0e1881bb3
Steven Zimmer <webdevzim@gmail.com>
2026-02-07 19:27:21 -0800

    change up

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,14 @@ import {LabelProvider} from "@/components/label-context";
 import {LabelNav} from "@/components/label-nav";
 export default function Home() {
     return (
-        <div className="font-sans dark:bg-black">
-            <LabelProvider>
-                <LabelNav />
-                {/* <LabelValidationWrapper /> */}
-            </LabelProvider>
+        <div className="font-sans">
+            {/* <LabelProvider> */}
+            {/* <LabelNav /> */}
+            {/* <LabelValidationWrapper /> */}
+            {/* </LabelProvider> */}
+            <div className="flex justify-center items-center w-full h-full py-20 bg-black text-white">
+                <div>Test empty state</div>
+            </div>
         </div>
     );
 }
